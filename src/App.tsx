8c20ba258eadc5e973ff2efa1d2c7482ef68e085
n27 <skfdkfkalsrn@gmail.com>
2022-03-30 14:06:38 +0900

    Commit before eject

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu';
+import { Route, Routes } from 'react-router-dom';
+import RedPage from './pages/RedPage';
+import BluePage from './pages/BluePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <div>
+      <Menu />
+      <hr />
+      <Routes>
+        <Route path='/red' element={<RedPage />} />
+        <Route path='/blue' element={<BluePage />} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
